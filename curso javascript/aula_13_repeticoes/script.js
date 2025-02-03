fb313f2c126d170fd60adcbd668d21b5bfc68241
ghoulswitch <die.millyturn@gmail.com>
2025-02-03 18:41:16 -0300

    estruturas de repetições

---
+++ curso javascript/aula_13_repeticoes/script.js
@@ -0,0 +1,14 @@
+var c = 1
+
+do {
+    console.log(`Tudo bem?`)
+    c++ 
+} while (c <= 6)
+
+/* var c = 1
+
+while (c <= 6) {
+    console.log(`Tudo bem?`)
+    //console.log(`Passo ${c}`)
+    c++ //c = c + 1
+} */
