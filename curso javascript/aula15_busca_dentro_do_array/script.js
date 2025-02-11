46ec8b189f75362ddbc74cffa68684550627818b
ghoulswitch <die.millyturn@gmail.com>
2025-02-11 19:59:27 -0300

    retornando o valor de um array ,,,

---
+++ curso javascript/aula15_busca_dentro_do_array/script.js
@@ -0,0 +1,9 @@
+let num = [5, 2, 8, 9, 3]
+
+let pos = num.indexOf(1)
+
+if (pos == -1) {
+    console.log(`O valor não foi encontrado!`)
+} else {
+    console.log(`O valor 8 está na posição ${pos}`)
+}
