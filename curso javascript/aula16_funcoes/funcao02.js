5ada94298e3c425ea9483f31e26eeccdc49efeda
ghoulswitch <die.millyturn@gmail.com>
2025-02-14 20:08:23 -0300

    funções: passagem de parâmetros + retorno de resultados

---
+++ curso javascript/aula16_funcoes/funcao02.js
@@ -0,0 +1,5 @@
+function soma(n1=0, n2=0) { // se não for passado nenhum valor, n1 e n2 serão 0
+    return n1 + n2
+}
+
+console.log(soma(2,5))
---
+++ curso javascript/aula16_funcoes/script.js
@@ -0,0 +1,12 @@
+function parimp(n) {
+    if (n%2 == 0) {
+        return 'par!'
+    } else {
+        return 'ímapar!'
+    }
+}
+
+//let res = parimp(4) 
+//console.log(res)
+
+console.log(parimp(4))
