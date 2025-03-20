6e01f7c69b193215848618194ea12444c92eb9ba
ghoulswitch <die.millyturn@gmail.com>
2025-03-20 17:33:35 -0300

    add: unions e interfaces em ts

---
+++ curso_typescript_felipe_rocha_youtube/objects.js
@@ -0,0 +1,18 @@
+//criando um objeto do tipo User; ao apertar ctrl+espaço o ts mostra as propriedades do objeto
+var usuario = {
+    firstName: "Milly",
+    age: 19,
+    email: "mowoono@gmail.com",
+    orders: [{ productId: "1", price: 100 }]
+};
+var author = {
+    age: 30,
+    books: ["Trono de vidro", "O príncipe cruel", "Tudo pelo jogo"],
+    email: "author@gmail.com",
+    firstName: "Sarah",
+    password: "123456"
+};
+
+// console.log(author);
+
+
--- curso_typescript_felipe_rocha_youtube/objects.ts
+++ curso_typescript_felipe_rocha_youtube/objects.ts
@@ -1,12 +1,13 @@
-//TYPE
+//TYPE - é um tipo de dado que poder ser usado para definir um objeto, array ou função
 type User = {
     firstName: string
     age: number
     email: string
     password?: string //o ponto de interrogação indica que a propriedade é opcional
-    orders: Orders[]
+    orders?: Orders[]
 };
 
+//criando um tipo de dado Orders que faz parte do tipo User 
 type Orders = {
     productId: string
     price: number
@@ -20,4 +21,28 @@ const usuario: User = {
     orders: [{productId: "1", price: 100}]
 };
 
-//UNIONS
+//UNIONS - é a união de dois tipos de dados
+type Author = {
+    books: string[]
+}
+
+const author: Author & User = {
+    age: 30,
+    books: ["Trono de vidro", "O príncipe cruel", "Tudo pelo jogo"],
+    email: "author@gmail.com",
+    firstName: "Sarah",
+    password: "123456"
+}
+
+// console.log(author);
+
+//INTERFACES
+interface IUser {
+    readonly firstName: string; //readonly indica que a propriedade não pode ser alterada; ou seja, é somente leitura 
+    email: string;
+}
+
+const mailUser: IUser = {
+    email: 'usuario@gmail.com',
+    firstName: 'Usuário'
+}
