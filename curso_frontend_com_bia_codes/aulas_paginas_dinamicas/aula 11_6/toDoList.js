a8a9800f6ffe534b1d107092d360269e90e55c88
ghoulswitch <die.millyturn@gmail.com>
2025-07-23 22:31:22 -0300

    add: aula: to do list + desafio (feito!)

--- curso_frontend_com_bia_codes/aulas_paginas_dinamicas/aula 11_6/toDoList.js
+++ curso_frontend_com_bia_codes/aulas_paginas_dinamicas/aula 11_6/toDoList.js
@@ -1,12 +1,32 @@
 const botao = document.querySelector('button');
+const botaoLimpar = document.getElementById("botaoLimpar");
+const inputValue = document.querySelector('input');
+const tasksList = document.querySelector('.tasksContainer'); //por tasksContainer ser uma classe é necessário adicionar o ponto antes!!!!
 
 botao.addEventListener('click', () => {
-    const tasksList = document.querySelector('.tasksContainer'); //por tasksContainer ser uma classe é necessário adicionar o ponto antes!!!!
-    const inputValue = document.querySelector('input');
-    const newTask = document.createElement('li');
-    newTask.innerText = inputValue.value;
+    if (inputValue.value) { //verifica se o input não está vazio, se estiver não adiciona nada
+        const newTask = document.createElement('li');
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
 
-    if (inputValue.value) //verifica se o input não está vazio, se estiver não adiciona nada
-    tasksList.appendChild(newTask);
-    inputValue.value = ''; //limpa o campo de input após adicionar a tarefa
+        newTask.appendChild(checkbox); //adicionando o checkbox primeiro
+
+        checkbox.addEventListener('click', () => {
+            if (checkbox.checked){
+                newTask.style.textDecoration = 'line-through dashed';
+            } else {
+                newTask.style.textDecoration = 'none';
+            }
+        })
+
+        const texto = document.createTextNode(inputValue.value); //cria um nó de texto com o valor do input, que será o texto da tarefa
+        newTask.appendChild(texto) //adiciona o texto ao lado do checkbox
+
+        tasksList.appendChild(newTask);
+        inputValue.value = ''; //limpa o campo de input após adicionar a tarefa
+    } 
+})
+
+botaoLimpar.addEventListener('click', () => {
+    tasksList.innerHTML = '';
 })
