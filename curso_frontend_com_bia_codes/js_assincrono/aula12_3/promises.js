9bda471643889a27fca2f186bd4d247c3bdfe173
ghoulswitch <die.millyturn@gmail.com>
2025-07-26 19:51:26 -0300

    add: aula: exemplo de Promise: fix

--- curso_frontend_com_bia_codes/js_assincrono/aula12_3/promises.js
+++ curso_frontend_com_bia_codes/js_assincrono/aula12_3/promises.js
@@ -1,6 +1,7 @@
 const catsFacts = () => {
-    fetch('https://meowfacts.herokuapp.com/').then((resposta) => resposta.json())
-    .then((batata) => console.log(batata));
+    fetch('https://meowfacts.herokuapp.com/') //o fetch retorna uma promise
+    .then((resposta) => resposta.json()) //converte a resposta par JSON, pois é o formato mais fácil de trabalhar
+    .then((batata) => console.log(batata)); //aqui 'batata' é o objeto JSON retornando pela API
 }
 
 catsFacts();
